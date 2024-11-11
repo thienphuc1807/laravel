fcf1af927e91389ade73e1b69930486da51abb17
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2024-11-12 03:56:25 +0700

    if else

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,9 +20,9 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <x-nav_link href="/"  :active="request()->is('/')">Home</x-nav_link>
-                                <x-nav_link href="/about"  :active="request()->is('about')">About</x-nav_link>
-                                <x-nav_link href="/contact"  :active="request()->is('contact')">Contact</x-nav_link>
+                                <x-nav_link href="/" type="a"  :active="request()->is('/')">Home</x-nav_link>
+                                <x-nav_link href="/about" type="a" :active="request()->is('about')">About</x-nav_link>
+                                <x-nav_link href="/contact" type="button" :active="request()->is('contact')">Contact</x-nav_link>
                             </div>
                         </div>
                     </div>
--- resources/views/components/nav_link.blade.php
+++ resources/views/components/nav_link.blade.php
@@ -1,7 +1,44 @@
-@props(["active" => false])
+@props(["active" => false, "type" => "a"])
 
+{{-- option 1 --}}
+{{-- <{{$type}} {{ $attributes }} 
+class="{{ $active ? 'rounded-md bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-white" 
+aria-current='{{ $active ? 'page' : 'false' }}'>
+{{ $slot }}
+</{{$type}} > --}}
+
+{{-- option 2 --}}
+{{-- <?php if($type === "a") : ?>
 <a {{ $attributes }} 
 class="{{ $active ? 'rounded-md bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-white" 
 aria-current='{{ $active ? 'page' : 'false' }}'>
 {{ $slot }}
 </a>
+<?php else : ?>
+<button {{ $attributes }} 
+class="{{ $active ? 'rounded-md bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-white" 
+aria-current='{{ $active ? 'page' : 'false' }}'>
+{{ $slot }}
+</button>
+<?php endif ?> --}}
+
+{{-- option 3 --}}
+@if($type === "a")
+<a {{ $attributes }} 
+class="{{ $active ? 'rounded-md bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-white" 
+aria-current='{{ $active ? 'page' : 'false' }}'>
+{{ $slot }}
+</a>
+@else :
+<button {{ $attributes }} 
+class="{{ $active ? 'rounded-md bg-gray-900' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-white" 
+aria-current='{{ $active ? 'page' : 'false' }}'>
+{{ $slot }}
+</button>
+@endif
+
+
+
+
+
+
